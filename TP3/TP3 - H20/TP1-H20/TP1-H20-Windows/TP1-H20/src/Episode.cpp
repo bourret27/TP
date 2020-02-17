9419c85489e8d2ca4a7429c18950681dd2238678
Philippe De Blois <philippe2blois@gmail.com>
2020-02-17 12:57:17 -0500

    Commit initial TP3

---
+++ TP3/TP3 - H20/TP1-H20/TP1-H20-Windows/TP1-H20/src/Episode.cpp
@@ -0,0 +1,37 @@
+// To do
+
+// To do
+Episode::Episode()
+// To do
+{
+}
+
+// To do
+Episode::Episode(unsigned int numEpisode, const std::string& nom, const std::string& duree)
+// To do
+{
+}
+
+// To do
+bool Episode::operator==(unsigned int numEpisode)
+{
+    // To do
+}
+
+// To do
+std::ostream& operator<<(std::ostream& os, const Episode& episode)
+{
+    // To do
+}
+
+// To do
+std::istream& operator>>(std::istream& is, Episode& episode)
+{
+    // To do
+}
+
+// To do
+unsigned int Episode::getNumEpisode() const
+{
+    // To do
+}
