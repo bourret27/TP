b637fc6a4e3f7ec3fcee8d8bbc0cce8085f75c1c
Philippe De Blois <philippe2blois@gmail.com>
2020-04-16 17:51:01 -0400

    Header guard sur Foncteurs.h

---
+++ TP5/TP5/src/Foncteurs.h
@@ -0,0 +1,12 @@
+/// Foncteurs en tous genres.
+/// \author William Bourret et Philippe De Blois
+/// \date 2020-04-16
+
+#ifndef FONCTEURS_H
+#define FONCTEURS_H
+
+// TODO : Mettre les includes nécéssaires
+
+
+
+#endif // FONCTEURS_H
