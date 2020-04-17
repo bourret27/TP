1f398615f2dfc88a00a7d6d82790045aa7ff6e49
William Bourret <46964762+bourret27@users.noreply.github.com>
2020-04-17 00:28:10 -0400

    Ajout des foncteurs dans Foncteurs.h

--- TP5/TP5/src/Foncteurs.h
+++ TP5/TP5/src/Foncteurs.h
@@ -6,7 +6,44 @@
 #define FONCTEURS_H
 
 // TODO : Mettre les includes nécéssaires
+#include <memory>
+#include <string>
+#include "Film.h"
+#include "LigneLog.h"
 
+class EstDansIntervallesDatesFilm {
+public:
+	EstDansIntervallesDatesFilm(int anneeInferieure, int anneeSuperieure) : anneeInferieure_(anneeInferieure), anneeSuperieure_(anneeSuperieure) {}
+	bool operator()(const std::unique_ptr<Film>& film) 
+	{
+		return film->annee >= anneeInferieure_ && film->annee <= anneeSuperieure_;
+	}
+
+private:
+	int anneeInferieure_;
+	int anneeSuperieure_;
+
+};
+
+class ComparateurLog {
+public:
+	bool operator(LigneLog logA, LigneLog logB) 
+	{
+		return logA.timestamp < logB.timestamp;
+	}
+
+};
+
+template<typename T1, typename T2>
+class ComparateurSecondElementPaire {
+public:
+	ComparateurSecondElementPaire();
+	bool operator(const std::pair<T1, T2>& pairA, const std::pair<T1, T2>& pairB)
+	{
+		return pairA.second < pairB.second;
+	}
+
+};
 
 
 #endif // FONCTEURS_H
