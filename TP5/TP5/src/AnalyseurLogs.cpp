5c9cd9a07a05a27632301e2901b140a0a65e23f7
William Bourret <46964762+bourret27@users.noreply.github.com>
2020-04-17 19:15:10 -0400

    Ajout des fonctions dans AnalyseurLogs.cpp

--- TP5/TP5/src/AnalyseurLogs.cpp
+++ TP5/TP5/src/AnalyseurLogs.cpp
@@ -54,3 +54,101 @@ bool AnalyseurLogs::chargerDepuisFichier(const std::string& nomFichier,
     std::cerr << "Erreur AnalyseurLogs: le fichier " << nomFichier << " n'a pas pu être ouvert\n";
     return false;
 }
+
+/// Crée une ligne de log et l'ajoute au vecteur.
+/// \param timestamp                La date du log.
+/// \param idUtilisateur            L'id de l'utilisateur dont le log est associé.
+/// \param nomFilm                  Le nom du film associé au log.
+/// \param gestionnaireUtilisateurs Référence au gestionnaire des utilisateurs pour lier un utilisateur à un log.
+/// \param gestionnaireFilms        Référence au gestionnaire des films pour pour lier un film à un log.
+/// \return                         True si la ligne a été ajoutée, false sinon.
+bool AnalyseurLogs::creerLigneLog(const std::string& timestamp, 
+                                  const std::string& idUtilisateur, 
+                                  const std::string& nomFilm,
+                                  GestionnaireUtilisateurs& gestionnaireUtilisateurs, 
+                                  GestionnaireFilms& gestionnaireFilms)
+{
+    if (gestionnaireUtilisateurs.getUtilisateurParId(idUtilisateur) == nullptr &&
+        gestionnaireFilms.getFilmParNom(nomFilm) == nullptr)
+    {
+        return false;
+    }
+    ajouterLigneLog({ timestamp, gestionnaireUtilisateurs.getUtilisateurParId(idUtilisateur), gestionnaireFilms.getFilmParNom(nomFilm) });
+    return true;
+}
+
+/// Ajoute une ligne de log au vecteur.
+/// \param ligneLog                La ligne de log à ajouter.
+void AnalyseurLogs::ajouterLigneLog(const LigneLog& ligneLog)
+{
+    ComparateurLog comparateur;
+    logs_.insert(std::upper_bound(logs_.begin(), logs_.end(), ligneLog, comparateur), ligneLog);
+    vuesFilms_[ligneLog.film]++;
+}
+
+/// Retourne le nombre de vues pour un film.
+/// \param film                     Le film dont on veut savoir le nombre de vues.
+/// \return                         Le nombre de vues pour ce film.
+int AnalyseurLogs::getNombreVuesFilm(const Film* film) const
+{
+    auto iterateurFilm = vuesFilms_.find(film);
+    if (iterateurFilm == vuesFilms_.end())
+    {
+        return -1;
+    }
+    return iterateurFilm->second;
+}
+
+/// Cherche le film le plus populaire.
+/// \return                         Un pointeur vers le film le plus populaire
+const Film* AnalyseurLogs::getFilmPlusPopulaire() const
+{
+    ComparateurSecondElementPaire <const Film*, int> comparateur;
+    if (logs_.size() == 0)
+    {
+        return nullptr;
+    }
+    return std::max_element(vuesFilms_.begin(), vuesFilms_.end(), comparateur)->first;
+}
+
+/// Cherche un certain nombre des films les plus populaires.
+/// \param nombre                   Le nombre de films désirés.
+/// \return                         Un vecteur de paires de films et de vues contenant les films les plus populaires
+std::vector<std::pair<const Film*, int>> AnalyseurLogs::getNFilmsPlusPopulaires(std::size_t nombre) const
+{
+    std::vector<std::pair<const Film*, int>> filmsPopulaires = std::vector<std::pair<const Film*, int>>(std::min(nombre, vuesFilms_.size()));
+    std::sort(vuesFilms_.begin(), vuesFilms_.end(), [](const std::pair<const Film*, int>& paireA, const std::pair<const Film*, int>& paireB) {
+        return paireA.second > paireB.second;
+        });
+    std::copy_n(vuesFilms_.begin(), std::min(nombre, vuesFilms_.size()), std::back_inserter(filmsPopulaires));
+    return filmsPopulaires;
+    
+
+}
+
+/// Retourne le nombre de vues pour un utilisateur.
+/// \param utilisateur              L'utilisateur dont on cherche le nombre de vues.
+/// \return                         Le nombre de vues pour un cet utilisateur.
+int AnalyseurLogs::getNombreVuesPourUtilisateur(const Utilisateur* utilisateur) const
+{
+    return std::count(logs_.begin(), logs_.end(), [utilisateur](std::vector<LigneLog>::const_iterator it)
+        {
+            return it->utilisateur->nom == utilisateur->nom;
+        });
+}
+
+/// Crée un vecteur des films vus par l'utilisateur.
+/// \param utilisateur              L'utilisateur dont on veut connaître les films vus.
+/// \return                         Un vecteur contenant tous les films vus par l'utilisateur.
+std::vector<const Film*> AnalyseurLogs::getFilmsVusParUtilisateur(const Utilisateur* utilisateur) const
+{
+    std::unordered_set<const Film*> filmsACopier;
+    for (LigneLog ligneLog : logs_)
+    {
+        if (ligneLog.utilisateur->nom == utilisateur->nom)
+        {
+            filmsACopier.insert(ligneLog.film);
+        }
+    }
+    return std::vector<const Film*>(filmsACopier.begin(), filmsACopier.end());
+}
